784f92d336beaa0cde5fbed29b3bbecd9bb3d15a
Seungwon Kim <seungwonkim@Seungwonui-MacBookAir.local>
2024-04-01 22:58:14 +0900

    fix: 코드의 구조를 수정하였습니다.

--- src/api/users/users.module.ts
+++ src/api/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { GroupsModule } from './modules/groups/groups.module';
+import { FollowsModule } from './modules/follows/follows.module';
 
 @Module({
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
+  imports: [GroupsModule, FollowsModule]
 })
 export class UsersModule {}
