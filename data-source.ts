299dfbb390e0d4e77aa188a78e6b947ba1a6ac45
Seungwon Kim <seungwonkim@Seungwonui-MacBookAir.local>
2024-04-03 16:30:35 +0900

    feat: 타입오알엠 마이그레이션을 생성하였습니다.

---
+++ data-source.ts
@@ -0,0 +1,21 @@
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+let envPath = 'envs/.local.env';
+dotenv.config({ path: path.resolve(envPath) });
+
+export default new DataSource({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: false,
+    entities: ['src/**/*.entity.ts'],
+    migrations: ['src/database/migrations/*.ts'],
+    migrationsTableName: 'migrations',
+});
---
+++ src/database/migrations/1712129231562-Migration.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1712129231562 implements MigrationInterface {
+    name = 'Migration1712129231562'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`nickname\` varchar(255) NOT NULL, \`phone\` varchar(255) NOT NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deleted_at\` datetime(6) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE \`users\``);
+    }
+
+}
